a2029e9579523c9b9d2d508917da93ff8548a485
Vikas Patil <evikas78@gmail.com>
2022-09-28 13:22:28 +0530

    initial commit

---
+++ src/main/java/demo/com/customer/CustromerProfile/CustromerProfileApplication.java
@@ -0,0 +1,13 @@
+package demo.com.customer.CustromerProfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustromerProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustromerProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/com/customer/CustromerProfile/CustromerProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.com.customer.CustromerProfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustromerProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
